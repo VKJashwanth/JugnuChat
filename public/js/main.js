1d346bff2fb58dc63163d0e9bc427e597a81bc92
VKJashwanth <141474787+VKJashwanth@users.noreply.github.com>
2023-12-15 21:12:38 +0000

    Minor Changes

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,10 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+const userList = document.getElementById('users')
 
 const { username, room } = Qs.parse(location.search, {
-	ignoreQueryPrefix: true,
+  ignoreQueryPrefix: true,
 });
 
 const socket = io();
@@ -15,54 +15,54 @@ socket.emit('joinRoom', { username, room });
 
 
 socket.on('roomUsers', ({ room, users }) => {
-	outputUsers(users);
-	outputRoomName(room);
+  outputUsers(users);
+  outputRoomName(room);
 });
 
 
 
 socket.on('message', message => {
-	console.log(message);
-	outputMessage(message);
-	chatMessages.scrollTop = chatMessages.scrollHeight;
+  console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener('submit', event => {
-	event.preventDefault();
-	const msg = event.target.elements.msg.value;
-	socket.emit('chatMessage', msg);
-	event.target.elements.msg.value = '';
-	event.target.elements.msg.focus();
+  event.preventDefault();
+  const msg = event.target.elements.msg.value;
+  socket.emit('chatMessage', msg);
+  event.target.elements.msg.value = '';
+  event.target.elements.msg.focus();
 });
 
 function outputMessage(message) {
-	const div = document.createElement('div');
-	div.classList.add('message');
-	var u = document.createElement("p");
-	var t = document.createElement("p");
-	u.classList.add("meta");
-	var uSp = document.createElement("span");
-	uSp.innerText = message.username + ' ';
-	var tSp = document.createElement("span");
-	tSp.innerText = message.time;
-	u.appendChild(uSp);
-	u.appendChild(tSp);
-	div.appendChild(u);
-	t.classList.add("text");
-	t.innerText = message.text;
-	div.appendChild(t);
-	document.querySelector('.chat-messages').appendChild(div);
+  const div = document.createElement('div');
+  div.classList.add('message');
+  var u = document.createElement("p");
+  var t = document.createElement("p");
+  u.classList.add("meta");
+  var uSp = document.createElement("span");
+  uSp.innerText = message.username + ' ';
+  var tSp = document.createElement("span");
+  tSp.innerText = message.time;
+  u.appendChild(uSp);
+  u.appendChild(tSp);
+  div.appendChild(u);
+  t.classList.add("text");
+  t.innerText = message.text;
+  div.appendChild(t);
+  document.querySelector('.chat-messages').appendChild(div);
 }
 
 function outputRoomName(room) {
-	roomName.innerText = room;
+  roomName.innerText = room;
 }
 
 function outputUsers(users) {
-	userList.innerHTML = "";
-	users.forEach(function(user) {
-		var li = document.createElement("li");
-		li.innerText = user.username;
-		userList.appendChild(li);
-	});
+  userList.innerHTML = "";
+  users.forEach(function(user) {
+    var li = document.createElement("li");
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
 }
